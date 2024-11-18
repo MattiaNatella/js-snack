e293df7712dcc1850487f3e0b239dd5207bcb75d
MattiaNatella <mattianatella@gmail.com>
2024-11-18 12:19:20 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JSnack 1
+// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+
+// 1. inserisco le variabili prompt
+// 2. verifico che siano numeri 
+// 3. li confronto e stampo il maggiore
