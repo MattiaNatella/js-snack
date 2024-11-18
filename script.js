b2ee91eb5026631c7aeadb2507b99c4f2f997c36
MattiaNatella <mattianatella@gmail.com>
2024-11-18 12:48:37 +0100

    Secondo snack completato

--- script.js
+++ script.js
@@ -4,3 +4,26 @@
 // 1. inserisco le variabili prompt
 // 2. verifico che siano numeri 
 // 3. li confronto e stampo il maggiore
+
+let primoNumero, secondoNumero, numeroMaggiore
+let numeriCorretti = false;
+
+while (!numeriCorretti) {
+  primoNumero = parseInt(prompt('Inserisci il primo numero'))
+  if (!isNaN(primoNumero)) {
+    secondoNumero = parseInt(prompt('Inserisci il secondo numero'))
+  }
+  if (!isNaN(secondoNumero)) {
+    numeriCorretti = true
+  }
+} 
+if (primoNumero > secondoNumero) {
+  numeroMaggiore = primoNumero
+} else if (secondoNumero > primoNumero){
+  numeroMaggiore = secondoNumero
+} else {
+  numeroMaggiore = 'i numeri sono uguali'
+}
+
+console.log('Numero maggiore = ', numeroMaggiore)
+
---
+++ secondo Snack/script.js
@@ -0,0 +1,27 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while (facoltativo)
+
+
+ let somma = 0;
+ let i = 0;
+
+ while (i !== 5) {
+  let inputNumeri = parseInt(prompt('inserisci un numero'))
+  somma += inputNumeri;
+  
+  i++
+}
+ console.log('La somma è pari a', somma)
+
+
+// ALTERNATIVA
+
+let sommatoria = 0;
+
+for (let i = 0; i < 5; i++) {
+  let insNumero = parseInt(prompt('Inserisci un numero'))
+  sommatoria += insNumero
+}
+
+console.log('la somma è pari a', sommatoria)
